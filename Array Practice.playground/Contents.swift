1f2ab860c11c6dca339714a75c472706cb206dfb
jolson615 <jolson615@gmail.com>
2018-07-08 17:05:00 -0400

    initial commit

---
+++ Array Practice.playground/Contents.swift
@@ -0,0 +1,51 @@
+//: Playground - noun: a place where people can play
+
+// Level 1:
+// Create an array of strings called favMovies that includes the titles of your five favorite movies.
+
+
+
+// Create an array of strings called myTable that includes the names of all the students at your table.
+
+
+
+// Create an array of strings called myClass that includes the names of all the students in your class.
+
+
+
+// Level 2:
+// Print out a sentence that describes your third favorite movie. Be sure to use the [] subscript notation to access the item within your array.
+
+
+// Without altering the code above, change your third favorite movie to something else.
+
+
+// Now print out your third favorite movie again to confirm that it actually changed.
+
+
+
+// Level 3:
+// Using a for-in loop, print out the names of all the people at your table.
+
+
+
+// Using a for-in loop, print out the names of all your favorite movies as part of complete sentences, for example, "I really love the movie _________!"
+
+
+
+
+// Level 4 STRETCH:
+// Using a for-in loop and a counter variable initialized before the loop starts, print out the names of each student in the class with a number in front of them. For example:
+// 1. Joe
+// 2. Cristian
+// 3. Zoe
+// etc...
+
+
+
+
+
+// Write a loop that iterates over an array and turns it into a sentence. for example, iterating over the array ["Moana", "Lilo & Stitch", "Coco"] should return the sentence "I really love the movies Moana, Lilo & Stitch, and Coco."
+
+
+
